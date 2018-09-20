fc818d5b2f34abde16139dba65155a3a7d30d57a
Varun <varunrajatkumar@gmail.com>
2018-09-21 01:48:42 +0530

    Home page design ready

---
+++ index.js
@@ -0,0 +1,9 @@
+function showSignUp(){
+    document.getElementsByClassName("sign-up")[0].style.display = "block";
+    document.getElementsByClassName("log-in")[0].style.display = "none";
+}
+
+function showLogIn(){
+    document.getElementsByClassName("log-in")[0].style.display = "block";
+    document.getElementsByClassName("sign-up")[0].style.display = "none";
+}
